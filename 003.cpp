f44f5c96ea761094c01c7c1f6830bdb8add3092a
SybilGrace <158843749+SybilGrace@users.noreply.github.com>
2025-10-27 11:07:54 -0500

    Random Number Guess Program
    Guess a number between 1 and 100.  
    This is a very standard beginner challenge.

---
+++ 003.cpp
@@ -0,0 +1,39 @@
+//Program to generate a random  number to guess a number between 1 and 100;
+//Writen by Sybil Grace, solution to practice application from the book 'C++ Programming' by D.S. Malik
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    //Declare and initialize variables
+    int solution;
+    int guess_count;
+    int guess;
+    bool answered;
+
+    answered = 0;
+    guess_count = 0;
+    srand(time(0));
+    solution = rand() % 100;
+
+    while (answered == 0) {
+        cout << "Guess a number between 1 and 100: " << endl;
+        cin >> guess;
+        cout <<  "Your most recent guess was: " << guess << endl;
+        guess_count = guess_count + 1;
+        if (guess < solution) {
+            cout <<  "Your guess was too low. Try again:" << endl;
+        }
+        else if (guess > solution) {
+            cout <<  "Your guess was too high. Try again:" << endl;
+        }
+        else {
+            answered = 1;
+        }
+    }
+    cout << "You figured out the solution: " << solution << endl;
+    cout << "It took you this many guesses: " << guess_count << endl;
+
+    return 0;
+}
